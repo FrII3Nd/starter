08431a48ad9df4667e3d85977c47dda196767912
FrII3Nd <88899668+FrII3Nd@users.noreply.github.com>
2026-04-25 17:15:31 +0300

    Update lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -14,7 +14,6 @@ end
 
 -- Вызываем функцию при старте
 setup_smart_compile_commands()
-
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 
@@ -22,9 +21,8 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.foldingRange = {
     dynamicRegistration = false,
     lineFoldingOnly = true
-},
+}
 vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
 vim.lsp.config("clangd", {
   cmd = {
     "clangd",
@@ -35,31 +33,24 @@ vim.lsp.config("clangd", {
     "--header-insertion=iwyu",
     "--header-insertion-decorators"
   },
-  options = {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-    cmd = { "clangd", "--background-index", "--clang-tidy" },
-  },
-
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
   filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
 })
 
-vim.lsp.enable "clangd"
-vim.lsp.enable "lua_ls"
 vim.g.clang_format_style = 'file'
 vim.g.clang_format_fallback_style = 'llvm'
 vim.g.clang_format_binary = "/home/linuxbrew/.linuxbrew/bin/clang-format"
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    configs.on_attach(client, args.buf)
-  end,
-})
+vim.lsp.enable("clangd")
+vim.lsp.enable("lua_ls")
 
 vim.api.nvim_create_user_command("Format", function(args)
   require("conform").format({ args = args.fargs, async = args.bang })
 end, { nargs = "*", bang = true, desc = "Format buffer using conform" })
+
+
 
 
